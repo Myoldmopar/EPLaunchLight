1ab31fc64bebfd0d85649980eacd79408a20b0fa
Edwin Lee <leeed2001@gmail.com>
2016-09-14 16:31:31 -0600

    Cleaning up window boxes, etc.

--- src/EPLaunchLightWindow.py
+++ src/EPLaunchLightWindow.py
@@ -15,7 +15,7 @@ class EPLaunchLightWindow(gtk.Window):
         super(EPLaunchLightWindow, self).__init__()
 
         # initialize some class-level "constants"
-        self.box_spacing = 3
+        self.box_spacing = 10
 
         # initialize instance variables to be set later
         self.input_file_path = None
@@ -24,7 +24,8 @@ class EPLaunchLightWindow(gtk.Window):
         self.button_cancel = None
         self.ep_run_folder = None
         self.running_simulation_thread = None
-        self.message_label = None
+        self.status_bar = None
+        self.status_bar_context_id = None
 
         # prepare threading
         gobject.threads_init()
@@ -44,13 +45,13 @@ class EPLaunchLightWindow(gtk.Window):
         self.set_position(gtk.WIN_POS_CENTER)
 
         # make a nice border around the outside of the window
-        self.set_border_width(10)
+        self.set_border_width(0)
 
         # set the window title
         self.set_title("EnergyPlus Launch Light")
 
         # set the background color
-        self.modify_bg(gtk.STATE_NORMAL, gtk.gdk.color_parse("#DB5700"))
+        # self.modify_bg(gtk.STATE_NORMAL, gtk.gdk.color_parse("#DB5700"))
 
         # add the body
         self.add(self.gui_build_body())
@@ -66,52 +67,70 @@ class EPLaunchLightWindow(gtk.Window):
         # create a vbox here first
         vbox = gtk.VBox(False, self.box_spacing)
 
+        # separator
+        vbox.pack_start(gtk.HSeparator(), False)
+
         # create the input file button and textbox section
         hbox1 = gtk.HBox(False, self.box_spacing)
         button1 = gtk.Button("Choose Input File..")
         button1.connect("clicked", self.select_input_file, FileTypes.IDF)
-        hbox1.pack_start(button1, False, False, self.box_spacing)
+        alignment = gtk.Alignment(xalign=1.0, yalign=0.5, xscale=1.0, yscale=0.5)
+        alignment.add(button1)
+        hbox1.pack_start(alignment, True, True, self.box_spacing)
         self.input_file_path = gtk.Entry()
         self.input_file_path.connect("changed", self.check_file_paths)
         self.input_file_path.set_text("/tmp/RefBldgHospitalNew2004_Chicago.idf")
         self.input_file_path.set_size_request(width=500, height=-1)
-        hbox1.pack_start(self.input_file_path, True, True, self.box_spacing)
-        vbox.pack_start(hbox1)
+        alignment = gtk.Alignment(xalign=1.0, yalign=0.5, xscale=1.0, yscale=0.5)
+        alignment.add(self.input_file_path)
+        hbox1.pack_start(alignment, True, True, self.box_spacing)
+        vbox.pack_start(hbox1, True, True, 0)
 
         # create the weather file button and textbox section
         hbox2 = gtk.HBox(False, self.box_spacing)
         button1 = gtk.Button("Choose Weather File..")
         button1.connect("clicked", self.select_input_file, FileTypes.EPW)
-        hbox2.pack_start(button1, False, False, self.box_spacing)
+        alignment = gtk.Alignment(xalign=1.0, yalign=0.5, xscale=1.0, yscale=0.5)
+        alignment.add(button1)
+        hbox2.pack_start(alignment, True, True, self.box_spacing)
         self.weather_file_path = gtk.Entry()
         self.weather_file_path.connect("changed", self.check_file_paths)
         self.weather_file_path.set_text("/Users/elee/EnergyPlus/repos/2eplus/weather/CZ06RV2.epw")
-        hbox2.pack_start(self.weather_file_path, True, True, self.box_spacing)
-        vbox.pack_start(hbox2)
+        self.weather_file_path.set_size_request(width=500, height=-1)
+        alignment = gtk.Alignment(xalign=1.0, yalign=0.5, xscale=1.0, yscale=0.5)
+        alignment.add(self.weather_file_path)
+        hbox2.pack_start(alignment, True, True, self.box_spacing)
+        vbox.pack_start(hbox2, True, True, 0)
+
+        # separator
+        vbox.pack_start(gtk.HSeparator(), False)
 
         # create the simulate/cancel button section
         hbox3 = gtk.HBox(False, self.box_spacing)
         self.button_sim = gtk.Button("Simulate")
         self.button_sim.connect("clicked", self.run_simulation)
-        alignment = gtk.Alignment(xalign=0.5, yalign=0.5, xscale=0.5, yscale=0.0)
+        alignment = gtk.Alignment(xalign=0.5, yalign=0.5, xscale=0.5, yscale=0.5)
         alignment.add(self.button_sim)
         hbox3.pack_start(alignment, True, True, self.box_spacing)
         self.button_cancel = gtk.Button("Cancel")
         self.button_cancel.connect("clicked", self.cancel_simulation)
-        alignment = gtk.Alignment(xalign=0.5, yalign=0.5, xscale=0.5, yscale=0.0)
+        alignment = gtk.Alignment(xalign=0.5, yalign=0.5, xscale=0.5, yscale=0.5)
         alignment.add(self.button_cancel)
         self.update_run_buttons(running=False)
         hbox3.pack_start(alignment, True, True, self.box_spacing)
-        vbox.pack_start(hbox3)
+        vbox.pack_start(hbox3, True, True, 0)
 
-        # create the weather file button and textbox section
-        hbox = gtk.HBox(False, self.box_spacing)
-        self.message_label = gtk.Label()
-        self.message_label.set_text("Ready for launch")
-        alignment = gtk.Alignment(xalign=0.5, yalign=0.5, xscale=0.5, yscale=0.0)
-        alignment.add(self.message_label)
-        hbox.pack_start(alignment, True, True, self.box_spacing)
-        vbox.pack_start(hbox)
+        # separator
+        vbox.pack_start(gtk.HSeparator(), False)
+
+        # create the status bar
+        self.status_bar = gtk.Statusbar()
+        self.status_bar.set_has_resize_grip(False)
+        self.status_bar_context_id = self.status_bar.get_context_id("Statusbar example")
+        self.status_bar.push(self.status_bar_context_id, "Ready for launch")
+        aligner = gtk.Alignment(1, 1, 1, 0)
+        aligner.add(self.status_bar)
+        vbox.pack_end(aligner, False, True, 0)
 
         # return the vbox
         return vbox
@@ -158,7 +177,7 @@ class EPLaunchLightWindow(gtk.Window):
         gobject.idle_add(self.message_handler, message)
 
     def message_handler(self, message):
-        self.message_label.set_text(message)
+        self.status_bar.push(self.status_bar_context_id, message)
 
     def completed_simulation(self, std_out):
         gobject.idle_add(self.completed_simulation_handler, std_out)
@@ -170,7 +189,7 @@ class EPLaunchLightWindow(gtk.Window):
         self.running_simulation_thread.stop()
 
     def check_file_paths(self, widget):
-        if self.weather_file_path is None or self.input_file_path is None or self.message_label is None:
+        if self.weather_file_path is None or self.input_file_path is None or self.status_bar is None:
             return  # we are probably doing early initialization of the GUI
         idf = self.input_file_path.get_text()
         epw = self.weather_file_path.get_text()
